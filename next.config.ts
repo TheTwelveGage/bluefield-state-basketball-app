fe2143d9b64a272825d0acaea94648a4f9b32d89
TheTwelveGage <119794326+TheTwelveGage@users.noreply.github.com>
2025-07-26 12:24:13 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,5 @@
 import type { NextConfig } from 'next'
 
-// @ts-ignore
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/gobstate\.com\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'bluefield-state-data',
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 24 * 60 * 60 // 24 hours
-        }
-      }
-    }
-  ]
-})
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -38,4 +17,4 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default withPWA(nextConfig)
+export default nextConfig
